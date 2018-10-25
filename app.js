d40bce3e6c182dd2bf7cc9ce896e7a32dfd6541c
Daworks <arete75@gmail.com>
2018-10-25 19:16:01 +0900

    반응형으로 수정

---
+++ app.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill';
+import './app.scss';
+
+window.feather = require( 'feather-icons' );
+
+window.goMonth = function ( index ) {
+	let tgs = document.querySelectorAll( '.month' );
+	tgs[ index ].scrollIntoView();
+}
+
+window.smoothscroll = function () {
+	let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+	if ( currentScroll > 0 ) {
+		window.requestAnimationFrame( smoothscroll );
+		window.scrollTo( 0, currentScroll - (currentScroll / 5) );
+	}
+};
+
+document.addEventListener( "DOMContentLoaded", function ( event ) {
+	feather.replace();
+} );
