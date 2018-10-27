846e5b2f7f2ed31fd55f22670b6ead0455736edc
Daworks <arete75@gmail.com>
2018-10-27 16:39:42 +0900

    강단색 적용

--- app.js
+++ app.js
@@ -17,4 +17,16 @@ window.smoothscroll = function () {
 
 document.addEventListener( "DOMContentLoaded", function ( event ) {
 	feather.replace();
+	let color = document.querySelectorAll('.color');
+	color.forEach(function(item){
+		if ( item.innerText.match(/흰색/) ) {
+			item.classList.add('bg-white');
+		} else if ( item.innerText.match(/녹색/) ) {
+			item.classList.add('bg-green');
+		} else if ( item.innerText.match(/보라색/) ) {
+			item.classList.add('bg-purple');
+		} else if ( item.innerText.match(/적색/) ) {
+			item.classList.add('bg-red');
+		}
+	})
 } );
